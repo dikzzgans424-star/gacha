d6af7116cc2cf94764ddfb3170b7bbedc8c3b78e
dikzzgans424-star <dikzzgans424@gmail.com>
2026-06-07 23:02:35 +0700

    Add Gacha function to fetch data from GitHub

---
+++ netlify/functions/gacha.js
@@ -0,0 +1,20 @@
+export async function handler() {
+  const token = process.env.GITHUB_TOKEN;
+
+  const res = await fetch(
+    "https://api.github.com/repos/dikzzgans424-star/slot/contents/gacha.json",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "application/vnd.github+json"
+      }
+    }
+  );
+
+  const data = await res.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  };
+    }
